5a58869d3682fee380058dbeaff1da5edd632473
stevechan <chanshunli@gmail.com>
2024-11-21 17:41:46 +0800

    Add scripts/video_script.sh

---
+++ scripts/video_script.sh
@@ -0,0 +1,5 @@
+# sendkeys apps => Code id:com.microsoft.VSCode
+sendkeys --application-name "Code" --characters "Summarize README.md"
+sendkeys --application-name "Code" --characters "How many files are there in the current directory?"
+sendkeys --application-name "Code" --characters "Who are you?"
+sendkeys --application-name "Code" --characters "Where is the chat API called?"
